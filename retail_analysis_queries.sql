c5b2757a1b9ee4a0b2ed8661f395a610e77d05d8
vmercy30 <vmercyerh@gmail.com>
2026-05-21 10:26:51 +0530

    Add files via upload

---
+++ retail_analysis_queries.sql
@@ -0,0 +1,16 @@
+
+# Retail Business Performance & Profitability Analysis
+
+## Objective
+Analyze retail transactional data to uncover profit-draining categories,
+optimize inventory turnover, and identify seasonal product behavior.
+
+## Tools Used
+- SQL
+- Python (Pandas, Seaborn, Matplotlib)
+- Tableau
+
+## Deliverables
+- Tableau Dashboard
+- SQL Queries
+- PDF Report
